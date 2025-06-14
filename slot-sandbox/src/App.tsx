693c443e7f0a4da86432f299635b365b18e2a323
adalhub <adalteran@gmail.com>
2025-06-14 11:51:47 -0500

    slot machine sandbowx webp added

---
+++ slot-sandbox/src/App.tsx
@@ -0,0 +1,10 @@
+import { GameSandbox } from './GameSandbox';
+
+export default function App() {
+  return (
+    <main>
+      <h1 style={{ textAlign: 'center' }}>Slot Sandbox</h1>
+      <GameSandbox />
+    </main>
+  );
+}
